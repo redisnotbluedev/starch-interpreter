1bbd7bbb6959f7e21c8979682f27e1aa9504ee52
redisnotbluedev <ededmaude@gmail.com>
2025-11-30 22:50:00 +0800

    what the hell is a constant man what

--- debug.c
+++ debug.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include "debug.h"
+#include "value.h"
 
 void disassembleChunk(Chunk* chunk, const char* name) {
 	printf("=== %s ===\n", name);
@@ -9,20 +10,30 @@ void disassembleChunk(Chunk* chunk, const char* name) {
 	}
 }
 
+static int constantInstruction(const char* name, Chunk* chunk, int offset) {
+	uint8_t constant = chunk->code[offset + 1];
+	printf("%-16s %4d '", name, constant);
+	printValue(chunk->constants.values[constant]);
+	printf("'\n");
+	return offset + 2;
+}
+
 static int simpleInstruction(const char* name, int offset) {
 	printf("%s\n", name);
 	return offset + 1;
 }
 
 int disassembleInstruction(Chunk* chunk, int offset) {
-  printf("%04d ", offset);
+	printf("%04d ", offset);
 
-  uint8_t instruction = chunk->code[offset];
-  switch (instruction) {
-    case OP_RETURN:
-      return simpleInstruction("OP_RETURN", offset);
-    default:
-      printf("Unknown opcode %d\n", instruction);
-      return offset + 1;
-  }
+	uint8_t instruction = chunk->code[offset];
+	switch (instruction) {
+		case OP_CONSTANT:
+			return constantInstruction("OP_CONSTANT", chunk, offset);
+		case OP_RETURN:
+			return simpleInstruction("OP_RETURN", offset);
+		default:
+			printf("Unknown opcode %d\n", instruction);
+			return offset + 1;
+	}
 }
--- main.c
+++ main.c
@@ -6,6 +6,11 @@ int main(int argc, const char* argv[]) {
 	// Create & init chunk
 	Chunk chunk;
 	initChunk(&chunk);
+
+	int constant = addConstant(&chunk, 1.2);
+	writeChunk(&chunk, OP_CONSTANT);
+	writeChunk(&chunk, constant);
+
 	// use opcode RETURN in the chunk
 	writeChunk(&chunk, OP_RETURN);
 
