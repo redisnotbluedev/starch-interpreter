d978bfe462e4e4f87b6777e3354a04fe617d40f0
redisnotbluedev <ededmaude@gmail.com>
2025-12-13 12:22:34 +1030

    functional scanner!!! (no lexical grammar tho)

--- scanner.c
+++ scanner.c
@@ -4,18 +4,49 @@
 #include "scanner.h"
 
 typedef struct {
-    // The start of the current lexeme
-    const char* start;
-    // The current character
-    const char* current;
-    // The line we're on
-    int line;
+	// The start of the current lexeme
+	const char* start;
+	// The current character
+	const char* current;
+	// The line we're on
+	int line;
 } Scanner;
 
 Scanner scanner;
 
 void initScanner(const char* source) {
-    scanner.start = source;
-    scanner.current = source;
-    scanner.line = 1;
+	scanner.start = source;
+	scanner.current = source;
+	scanner.line = 1;
+}
+
+static bool isAtEnd() {
+	return *scanner.current == '\0';
+}
+
+static Token makeToken(TokenType type) {
+	Token token;
+	token.type = type;
+	token.start = scanner.start;
+	token.length = (int)(scanner.current - scanner.start);
+	token.line = scanner.line;
+	return token;
+}
+
+static Token errorToken(const char* message) {
+	Token token;
+	token.type = TOKEN_ERROR;
+	token.start = message;
+	token.length = (int)strlen(message);
+	token.line = scanner.line;
+	return token;
+}
+
+Token scanToken() {
+	// Each call to the function scans a complete token
+	scanner.start = scanner.current;
+
+	if (isAtEnd()) return makeToken(TOKEN_EOF);
+
+	return errorToken("Unexpected character.");
 }
