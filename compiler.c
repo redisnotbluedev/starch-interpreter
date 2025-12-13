87086de0c4294ebfe97316f79d99686e238c3b69
redisnotbluedev <ededmaude@gmail.com>
2025-12-13 21:51:08 +1030

    comments

--- compiler.c
+++ compiler.c
@@ -5,22 +5,32 @@
 #include "scanner.h"
 
 typedef struct {
+	// Current token
 	Token current;
+	// Previous token
 	Token previous;
+	// Whether the parser's run into an error
 	bool hadError;
+	// panicMode supresses any extra errors, so only one error is outputted 
 	bool panicMode;
 } Parser;
 
 Parser parser;
+// The current chunk
 Chunk* compilingChunk;
 
+// Output the current chunk
+/* This is a function because later the logic will
+   be more messy and we'll need more than just a return */
 static Chunk* currentChunk() {
 	return compilingChunk;
 }
 
 static void errorAt(Token* token, const char* message) {
+	// Supress more warnings if panic mode is on
 	if (parser.panicMode) return;
 	parser.panicMode = true;
+	// Print out the error
 	fprintf(stderr, "[line %d] Error", token->line);
 
 	if (token->type == TOKEN_EOF) {
@@ -31,70 +41,93 @@ static void errorAt(Token* token, const char* message) {
 		fprintf(stderr, " at '%.*s'", token->length, token->start);
 	}
 
+	// Message
 	fprintf(stderr, ": %s\n", message);
 	parser.hadError = true;
 }
 
+// Create an error
 static void error(const char* message) {
 	errorAt(&parser.previous, message);
 }
 
-static void error(const char* message) {
-	errorAt(&parser.previous, message);
-}
-
+// Create an error at the current token
 static void errorAtCurrent(const char* message) {
 	errorAt(&parser.current, message);
 }
 
+// Advance to the next token
 static void advance() {
 	parser.previous = parser.current;
 
+	// Repeat indefinitely
 	for (;;) {
+		// Get a token
 		parser.current = scanToken();
+		// If it not an error, finish
 		if (parser.current.type != TOKEN_ERROR) break;
-
+		// Otherwise create an error
 		errorAtCurrent(parser.current.start);
 	}
 }
 
+// Look for a token type, but raise an error if it's not there
 static void consume(TokenType type, const char* message) {
 	if (parser.current.type == type) {
+		// Eat the token
 		advance();
 		return;
 	}
 
+	// Otherwise raise an error
 	errorAtCurrent(message);
 }
 
+// Create bytecode
 static void emitByte(uint8_t byte) {
 	writeChunk(currentChunk(), byte, parser.previous.line);
 }
 
+// Create TWO bytes of bytecode
+// Yes, TWO (2) whole bytes
+// That's like my whole disk space
+// Absolutely massive to be honest
+// I mean I can't even count that high really
 static void emitBytes(uint8_t byte1, uint8_t byte2) {
 	emitByte(byte1);
 	emitByte(byte2);
 }
 
+// Emit... a return token
+// Pretty simple stuff lads, nothing to see here
 static void emitReturn() {
+	// The reason behind this is because OP_RETURN doesn't do return things right now
+	// When we get functions this won't be needed
 	emitByte(OP_RETURN);
 }
 
+// Really self-explanatory
+// Is anyone reading these?
 static void endCompiler() {
 	emitReturn();
 }
 
+// Bop it! Twist it! Pull it! Compile it!
 bool compile(const char* source, Chunk* chunk) {
+	// initialise values
 	initScanner(source);
 	compilingChunk = chunk;
 
 	parser.hadError = false;
 	parser.panicMode = false;
 
+	// Compile an expression since this is too dumb to handle statements
+	// (yet)
 	advance();
 	expression();
 	consume(TOKEN_EOF, "Expect end of expression.");
 
+	// Clean up
 	endCompiler();
 	return !parser.hadError;
 }
