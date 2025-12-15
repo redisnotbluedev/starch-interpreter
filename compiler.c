8ee2fc57b40ef3015741854e1f9144e9ea3c58d8
redisnotbluedev <ededmaude@gmail.com>
2025-12-15 14:26:56 +1030

    pratt the parser

--- compiler.c
+++ compiler.c
@@ -29,6 +29,18 @@ typedef enum {
   	PREC_PRIMARY
 } Precedence;
 
+// A function that takes nothing and returns nothing
+typedef void (*ParseFn)();
+
+typedef struct {
+	// Function for prefix
+	ParseFn prefix;
+	// Function for infix
+	ParseFn infix;
+	// Its precedence
+	Precedence precedence;
+} ParseRule;
+
 Parser parser;
 // The current chunk
 Chunk* compilingChunk;
@@ -157,14 +169,81 @@ static void unary() {
 	}
 }
 
+ParseRule rules[] = {
+	[TOKEN_LEFT_PAREN]    = {grouping, NULL,   PREC_NONE},
+	[TOKEN_RIGHT_PAREN]   = {NULL,     NULL,   PREC_NONE},
+	[TOKEN_LEFT_BRACE]    = {NULL,     NULL,   PREC_NONE}, 
+	[TOKEN_RIGHT_BRACE]   = {NULL,     NULL,   PREC_NONE},
+	[TOKEN_COMMA]         = {NULL,     NULL,   PREC_NONE},
+	[TOKEN_DOT]           = {NULL,     NULL,   PREC_NONE},
+	[TOKEN_MINUS]         = {unary,    binary, PREC_TERM},
+	[TOKEN_PLUS]          = {NULL,     binary, PREC_TERM},
+	[TOKEN_SEMICOLON]     = {NULL,     NULL,   PREC_NONE},
+	[TOKEN_SLASH]         = {NULL,     binary, PREC_FACTOR},
+	[TOKEN_STAR]          = {NULL,     binary, PREC_FACTOR},
+	[TOKEN_BANG]          = {NULL,     NULL,   PREC_NONE},
+	[TOKEN_BANG_EQUAL]    = {NULL,     NULL,   PREC_NONE},
+	[TOKEN_EQUAL]         = {NULL,     NULL,   PREC_NONE},
+	[TOKEN_EQUAL_EQUAL]   = {NULL,     NULL,   PREC_NONE},
+	[TOKEN_GREATER]       = {NULL,     NULL,   PREC_NONE},
+	[TOKEN_GREATER_EQUAL] = {NULL,     NULL,   PREC_NONE},
+	[TOKEN_LESS]          = {NULL,     NULL,   PREC_NONE},
+	[TOKEN_LESS_EQUAL]    = {NULL,     NULL,   PREC_NONE},
+	[TOKEN_IDENTIFIER]    = {NULL,     NULL,   PREC_NONE},
+	[TOKEN_STRING]        = {NULL,     NULL,   PREC_NONE},
+	[TOKEN_NUMBER]        = {number,   NULL,   PREC_NONE},
+	[TOKEN_AND]           = {NULL,     NULL,   PREC_NONE},
+	[TOKEN_CLASS]         = {NULL,     NULL,   PREC_NONE},
+	[TOKEN_ELSE]          = {NULL,     NULL,   PREC_NONE},
+	[TOKEN_FALSE]         = {NULL,     NULL,   PREC_NONE},
+	[TOKEN_FOR]           = {NULL,     NULL,   PREC_NONE},
+	[TOKEN_FUN]           = {NULL,     NULL,   PREC_NONE},
+	[TOKEN_IF]            = {NULL,     NULL,   PREC_NONE},
+	[TOKEN_NIL]           = {NULL,     NULL,   PREC_NONE},
+	[TOKEN_OR]            = {NULL,     NULL,   PREC_NONE},
+	[TOKEN_PRINT]         = {NULL,     NULL,   PREC_NONE},
+	[TOKEN_RETURN]        = {NULL,     NULL,   PREC_NONE},
+	[TOKEN_SUPER]         = {NULL,     NULL,   PREC_NONE},
+	[TOKEN_THIS]          = {NULL,     NULL,   PREC_NONE},
+	[TOKEN_TRUE]          = {NULL,     NULL,   PREC_NONE},
+	[TOKEN_VAR]           = {NULL,     NULL,   PREC_NONE},
+	[TOKEN_WHILE]         = {NULL,     NULL,   PREC_NONE},
+	[TOKEN_ERROR]         = {NULL,     NULL,   PREC_NONE},
+	[TOKEN_EOF]           = {NULL,     NULL,   PREC_NONE},
+};
+
 static void parsePrecedence(Precedence precedence) {
 
+}
+
+static ParseRule* getRule(TokenType type) {
+	// Return the rule at the given index
+	return &rules[type];
 }
 
 // Really self-explanatory
 // Is anyone reading these?
 static void endCompiler() {
 	emitReturn();
+}
+
+// Forward declarations to handle recursive grammar
+static void expression();
+static ParseRule* getRule(TokenType type);
+static void parsePrecedence(Precedence precedence);
+
+static void binary() {
+	TokenType operatorType = parser.previous.type;
+	ParseRule* rule = getRule(operatorType);
+	parsePrecedence((Precedence)(rule->precedence + 1));
+
+	switch (operatorType) {
+		case TOKEN_PLUS:   emitByte(OP_ADD); break;
+		case TOKEN_MINUS:  emitByte(OP_SUBTRACT); break;
+		case TOKEN_STAR:   emitByte(OP_MULTIPLY); break;
+		case TOKEN_SLASH:  emitByte(OP_DIVIDE); break;
+		default: return; // Unreachable.
+	}
 }
 
 static void grouping() {
