787617acb411eba8fcec681ac12acb964d9b1f2c
redisnotbluedev <red@redisnot.blue>
2026-06-01 20:23:31 +0930

    does it work with failures?

--- tests/lexer/comments.nim
+++ tests/lexer/comments.nim
@@ -10,5 +10,7 @@ suite "Lexing: Comments":
         assertTokens("//single",        [(TokenType.comment, "single", 0, 8)])
     test "Block comment":
         assertTokens("/*multi\nline*/", [(TokenType.comment, "multi\nline", 0, 14)])
+    test "dummy test":
+        assertTokens("/*multi\nline*/", [(TokenType.comment, "dummy", 3, 42)])
     test "Unterminated block comment":
         assertError("/* doesn't end",   StarchSyntaxError,   "unterminated block comment", 1, 1, 14)
